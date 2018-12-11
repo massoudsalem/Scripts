b1af43495a52778d1662fb9e82b70689bd3faaf2
massoudsalem <massoudsalem@users.noreply.github.com>
2018-12-11 22:17:38 +0200

    Adding Drag and Drop using pyinstaller

--- PDFMarge.py
+++ PDFMarge.py
@@ -1,3 +1,7 @@
+"""/*******************************************
+ * - Coded by Moh.Massoud
+ * - Problem: Merging multiple pdf files togrther into single one
+ ********************************************/"""
 import sys
 from PyPDF2 import PdfFileMerger, PdfFileReader
 merge=PdfFileMerger()
@@ -10,3 +14,8 @@ for file in sys.argv[1:]:
     if x:
         merge.write("output.pdf")
 print ("Success\n" if x==1 else "Fail\n")
+"""Note:***********************************************************
+        you can use pyinstaller to make an excutable .exe file
+        so you can drag and drop the pdf files into it's icon
+        and the output pdf will appear next to itself.
+***************************************************************/"""
